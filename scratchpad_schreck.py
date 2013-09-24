2a5903209a9e6eda41ad70ea8d3f25f3b11fafe5
Aaron Andalman <nerduno.list@gmail.com>
2013-09-24 13:58:38 -0700

    Updated schreck batch processing.

--- scratchpad_schreck.py
+++ scratchpad_schreck.py
@@ -6,37 +6,77 @@ import matplotlib.pyplot as pyplot
 import pylab
 import scipy
 import math
+import itertools
 
-f_pre = ['/home/vburns/Dropbox/ConchisData/2013-07-10/f00601/f00601_2013-07-10-14-32-59.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00602/f00602_2013-07-10-14-32-59.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00603/f00603_2013-07-10-14-32-59.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00604/f00604_2013-07-10-14-32-59.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00605/f00605_2013-07-10-14-32-59.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00606/f00606_2013-07-10-14-32-59.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00607/f00607_2013-07-10-14-33-00.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00608/f00608_2013-07-10-14-33-00.json']
+f_pre = ['/Users/andalman/Dropbox/ConchisData/2013-07-10/f00601/f00601_2013-07-10-14-32-59.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00602/f00602_2013-07-10-14-32-59.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00603/f00603_2013-07-10-14-32-59.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00604/f00604_2013-07-10-14-32-59.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00605/f00605_2013-07-10-14-32-59.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00606/f00606_2013-07-10-14-32-59.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00607/f00607_2013-07-10-14-33-00.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00608/f00608_2013-07-10-14-33-00.json']
 
-f_veh= ['/home/vburns/Dropbox/ConchisData/2013-07-10/f00601/f00601_2013-07-10-15-05-42.json',
-        '/home/vburns/Dropbox/ConchisData/2013-07-10/f00602/f00602_2013-07-10-15-05-42.json',
-        '/home/vburns/Dropbox/ConchisData/2013-07-10/f00603/f00603_2013-07-10-15-05-42.json',
-        '/home/vburns/Dropbox/ConchisData/2013-07-10/f00604/f00604_2013-07-10-15-05-42.json',
-        '/home/vburns/Dropbox/ConchisData/2013-07-10/f00605/f00605_2013-07-10-15-05-42.json',
-        '/home/vburns/Dropbox/ConchisData/2013-07-10/f00606/f00606_2013-07-10-15-05-43.json',
-        '/home/vburns/Dropbox/ConchisData/2013-07-10/f00607/f00607_2013-07-10-15-05-43.json',
-        '/home/vburns/Dropbox/ConchisData/2013-07-10/f00608/f00608_2013-07-10-15-05-43.json']
+f_veh= ['/Users/andalman/Dropbox/ConchisData/2013-07-10/f00601/f00601_2013-07-10-15-05-42.json',
+        '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00602/f00602_2013-07-10-15-05-42.json',
+        '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00603/f00603_2013-07-10-15-05-42.json',
+        '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00604/f00604_2013-07-10-15-05-42.json',
+        '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00605/f00605_2013-07-10-15-05-42.json',
+        '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00606/f00606_2013-07-10-15-05-43.json',
+        '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00607/f00607_2013-07-10-15-05-43.json',
+        '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00608/f00608_2013-07-10-15-05-43.json']
 
-f_sch = ['/home/vburns/Dropbox/ConchisData/2013-07-10/f00601/f00601_2013-07-10-15-37-43.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00602/f00602_2013-07-10-15-37-43.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00603/f00603_2013-07-10-15-37-43.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00604/f00604_2013-07-10-15-37-43.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00605/f00605_2013-07-10-15-37-43.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00606/f00606_2013-07-10-15-37-44.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00607/f00607_2013-07-10-15-37-44.json',
-         '/home/vburns/Dropbox/ConchisData/2013-07-10/f00608/f00608_2013-07-10-15-37-44.json']
+f_sch = ['/Users/andalman/Dropbox/ConchisData/2013-07-10/f00601/f00601_2013-07-10-15-37-43.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00602/f00602_2013-07-10-15-37-43.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00603/f00603_2013-07-10-15-37-43.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00604/f00604_2013-07-10-15-37-43.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00605/f00605_2013-07-10-15-37-43.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00606/f00606_2013-07-10-15-37-44.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00607/f00607_2013-07-10-15-37-44.json',
+         '/Users/andalman/Dropbox/ConchisData/2013-07-10/f00608/f00608_2013-07-10-15-37-44.json']
 
 d_pre = np.array(aba.loadMultipleDataFiles(f_pre, arena_mm = None))
 d_veh = np.array(aba.loadMultipleDataFiles(f_veh, arena_mm = None))
 d_sch = np.array(aba.loadMultipleDataFiles(f_sch, arena_mm = None))
+
+####### QUANTIFY CENTER TYPE
+whichndx = [1,2,3,4,5,6,7]
+compare = [[d_pre[whichndx],d_veh[whichndx]],[d_veh[whichndx], d_sch[whichndx]]]
+trange = [[[1000,1700],[0,360]],  [[1000,1700],[0,360]]]
+titles = ['Vehicle','Schreckstoff']
+labels = [['Baseline','Vehicle_Start'],['Vehicle_End','Schreckstoff_Start']]
+bins = np.linspace(0,300,50)
+f = pyplot.figure(7)
+f.set_facecolor('w')
+mmperpixel=35.0/300
+center=(150,150)
+center_rad = 100
+for compnum,comp in enumerate(compare):
+    timeInCenter = np.zeros([len(comp),len(comp[0])])
+    for condnum, cond in enumerate(comp):
+        for fishnum, fish in enumerate(cond):
+            tracking = aba.getTracking(fish)
+            tracking[:,1]-=tracking[:,1].min()
+            tracking[:,2]-=tracking[:,2].min()
+            trel = trange[compnum][condnum] + tracking[0,0]
+            tndx = np.logical_and(tracking[:,0]>=trel[0], tracking[:,0]<trel[1])
+            heatmap, xedges, yedges = np.histogram2d(tracking[tndx,1],tracking[tndx,2],bins=bins,normed=True)
+            xx,yy = np.meshgrid([x1+x2/2.0 for x1, x2 in itertools.izip(xedges[1:], xedges)],
+                                [y1+y2/2.0 for y1, y2 in itertools.izip(yedges[1:], yedges)])
+            xxndx,yyndx = np.meshgrid(range(len(xedges)-1),range(len(yedges)-1))
+            centerndx = np.nonzero(np.sqrt(pow(xx-center[0],2)+pow(yy-center[1],2))<center_rad)
+            timeInCenter[condnum][fishnum] = heatmap[xxndx[centerndx],yyndx[centerndx]].sum() / heatmap.sum()
+    print timeInCenter
+    (t,p) = scipy.stats.ttest_rel(timeInCenter[0,:],timeInCenter[1,:])
+    pyplot.subplot(1,len(compare),1+compnum)
+    pyplot.plot(range(len(comp)),timeInCenter,'o-b')
+    pyplot.xlim([-0.5,len(comp)-0.5])
+    pyplot.title('%s (paired t-test p=%f)'%(titles[compnum],p))
+    pyplot.gca().xaxis.set_ticks(range(len(comp)))
+    pyplot.gca().xaxis.set_ticklabels(labels[compnum])
+    pyplot.ylabel('Prob in Center')
+
+pyplot.show()
 
 ####### getMedianVelocity
 whichndx = [0,1,2,3,4,5,6,7]
@@ -87,7 +127,7 @@ for i,d in enumerate([d_pre[whichndx],d_veh[whichndx],d_veh[whichndx], d_sch[whi
         tracking[:,1]-=tracking[:,1].min()
         tracking[:,2]-=tracking[:,2].min()
         aba.plotFishPathHeatmap(f,trange[i],bins=bins,tracking=tracking)
-        pyplot.savefig('/home/vburns/Dropbox/ConchisData/Schreckstoff/20130710_%s_heatmap_%dto%d.png'%
+        pyplot.savefig('/Users/andalman/Dropbox/ConchisData/Schreckstoff/20130710_%s_heatmap_%dto%d.png'%
                        (titles[i],trange[i][0],trange[i][1]), dpi=300)
 
 pyplot.show()
